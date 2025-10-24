f2c7de5994c6cc551d1d99391aafee792c817317
Ananya Agarwal <ananyaagarwal112@gmail.com>
2025-10-25 00:58:01 +0530

    Add files via upload

---
+++ Q081.c
@@ -0,0 +1,28 @@
+//Q81: Count characters in a string without using built-in length functions.
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+ 
+Output 2:
+1
+*/
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[400];
+    printf("Enter the string: ");
+    gets(str);
+    int i=0;
+    while (str[i]!='\0')
+    {
+        i++;
+    }
+    printf("Characters in the string is: %d", i);
+    return 0;
+}
